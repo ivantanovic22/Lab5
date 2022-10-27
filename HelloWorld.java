8264bc845d03c8a1a3d46326d35317f352f07b34
Ubuntu <ubuntu@ip-172-31-30-168.ec2.internal>
2022-10-27 09:49:26 +0000

    update

--- HelloWorld.java
+++ HelloWorld.java
@@ -7,5 +7,10 @@ class HelloWorld
     public static void main(String args[]) 
     { 
         System.out.println("Hello, World"); 
-    } 
+    }
+
+    {
+        System.out.println("Goodbye, World");
+    }
+ 
 }
